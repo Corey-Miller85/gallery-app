cde8b36d93baf2f0ef615122a4a55282f4930128
Corey Miller <coreyMiller353@gmail.com>
2019-12-05 20:40:53 -0500

    add state to searchbar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -5,21 +5,28 @@ class SearchBar extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      something: null
+      searchValue: ""
     }
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleSearch(evt)  {
     evt.preventDefault();
     // TODO: Make this work right
-    console.log("Clicked on Search")
+  }
+
+  handleChange(evt) {
+    const target = evt.target.value;
+    this.setState({
+      searchValue: target
+    })
   }
 
   render() {
     return (
       <div>
         <form className="SearchBar">
-          <input className="SearchBar-input" type="text" placeholder="Search images..."/>
+          <input className="SearchBar-input" type="text" placeholder="Search images..." value={this.state.searchValue} onChange={this.handleChange}/>
           <button className="SearchBar-button" type="submit" onClick={this.handleSearch}></button>
         </form>
       </div>
