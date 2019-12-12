c2b337c8d67f4622f45c1ffa39a3536ad0b6e169
Corey Miller <coreyMiller353@gmail.com>
2019-12-12 11:51:57 -0500

    seach bar and nav links not rendering new photos

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,26 +1,24 @@
-import React, {Component} from 'react'
-import '../CSS/NavBar.css'
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import "../CSS/NavBar.css";
 
 class NavBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-    }
-  }
-
-  render() {
-    return (
-      <div className="NavBar">
-        <h1>Flikr Search App</h1>
-        <ul>
-          <li><a>List item 1</a></li>
-          <li><a>List item 2</a></li>
-        </ul>
-      </div>
-    )
-  }
-
+	render() {
+		return (
+			<div className='NavBar'>
+				<h1>Flikr Search App</h1>
+				<ul>
+					<NavLink to='/trees'>List Item 1</NavLink>
+					<NavLink to='/bears'>List Item 2</NavLink>
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default NavBar;
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,37 +1,50 @@
-import React, { Component } from 'react'
-import '../CSS/SearchBar.css'
+import React, { Component } from "react";
+import "../CSS/SearchBar.css";
 
 class SearchBar extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      searchValue: ""
-    }
-    this.handleChange = this.handleChange.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			searchValue: ""
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSearch = this.handleSearch.bind(this);
+	}
 
-  handleSearch(evt)  {
-    evt.preventDefault();
-    // TODO: Make this work right
-  }
+	handleSearch(evt) {
+		evt.preventDefault();
+		let searchValue = this.state.searchValue;
+		let url = `/${searchValue}`;
+		this.props.history.push(url);
+	}
 
-  handleChange(evt) {
-    const target = evt.target.value;
-    this.setState({
-      searchValue: target
-    })
-  }
+	handleChange(evt) {
+		const target = evt.target.value;
+		this.setState({
+			searchValue: target
+		});
+	}
 
-  render() {
-    return (
-      <div>
-        <form className="SearchBar">
-          <input className="SearchBar-input" type="text" placeholder="Search images..." value={this.state.searchValue} onChange={this.handleChange}/>
-          <button className="SearchBar-button" type="submit" onClick={this.handleSearch}></button>
-        </form>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<form className='SearchBar' onSubmit={this.handleSearch}>
+					<input
+						className='SearchBar-input'
+						type='text'
+						placeholder='Search images...'
+						value={this.state.searchValue}
+						onChange={this.handleChange}
+					/>
+					<button
+						className='SearchBar-button'
+						type='submit'
+						onClick={this.handleSearch}
+					></button>
+				</form>
+			</div>
+		);
+	}
 }
 
-export default SearchBar
+export default SearchBar;
