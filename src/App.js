82bc9ca8514cc8cec17acbec17fec5282e402d73
Corey Miller <coreyMiller353@gmail.com>
2019-12-12 11:26:57 -0500

    add route for id in url

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, {Component} from 'react'
-import Gallery from './Components/Gallery'
-import './App.css'
+import React, { Component } from "react";
+import Gallery from "./Components/Gallery";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
-class App extends Component { 
-  render(){
-    return(
-      <Gallery />
-    )
-  }
+class App extends Component {
+	render() {
+		return (
+			<Switch>
+				<Route
+					exact
+					path='/'
+					render={routerProps => <Gallery {...routerProps} />}
+				/>
+				<Route
+					exact
+					path='/:id'
+					render={routerProps => <Gallery {...routerProps} />}
+				/>
+			</Switch>
+		);
+	}
 }
 
 export default App;
--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,60 +1,66 @@
-import React, { Component } from 'react';
-import {
-  Route,
-  BrowserRouter
-} from 'react-router-dom'
-import Photo from './Photo'
-import NavBar from './NavBar'
-import SearchBar from './SearchBar'
-import axios from 'axios'
-import apiKey from './Config'
-import '../CSS/Gallery.css'
+import React, { Component } from "react";
 
+import Photo from "./Photo";
+import NavBar from "./NavBar";
+import SearchBar from "./SearchBar";
+import axios from "axios";
+import apiKey from "./Config";
+import "../CSS/Gallery.css";
 
 class Gallery extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      photos: []
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			photos: []
+		};
+	}
 
-  componentDidMount() {
-    this.getPhotos();
-  }
+	componentDidMount() {
+		this.getPhotos();
+	}
 
-  getPhotos() {
-    let res = axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=forest&per_page=24&format=json&nojsoncallback=1`)
-    .then(response => this.setState({
-     photos: response.data.photos.photo
-    }))
-  }
+	getPhotos() {
+		let res = axios
+			.get(
+				`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${this.props.match.params.id}&per_page=24&format=json&nojsoncallback=1`
+			)
+			.then(response =>
+				this.setState({
+					photos: response.data.photos.photo
+				})
+			);
+	}
 
-  render() {
-    let photos = this.state.photos.map((item, index) => {
-      let farm = item.farm;
-      let server = item.server;
-      let id = item.id;
-      let secret = item.secret;
-      let title = item.title;
-      return <Photo key={id} farm={farm} server={server} id={id} secret={secret} title={title}/> 
-    })
-    return (
-      <BrowserRouter>
-        <div className="Gallery">
-          <div className="Gallery-search">
-            <SearchBar />
-          </div>
-          <div className="Gallery-nav">
-            <NavBar />
-          </div>
-          <ul>
-            {photos}
-          </ul>
-        </div>
-      </BrowserRouter>
-    )
-  }
+	render() {
+		let photos = this.state.photos.map((item, index) => {
+			let farm = item.farm;
+			let server = item.server;
+			let id = item.id;
+			let secret = item.secret;
+			let title = item.title;
+			return (
+				<Photo
+					key={id}
+					farm={farm}
+					server={server}
+					id={id}
+					secret={secret}
+					title={title}
+				/>
+			);
+		});
+		return (
+			<div className='Gallery'>
+				<div className='Gallery-search'>
+					<SearchBar />
+				</div>
+				<div className='Gallery-nav'>
+					<NavBar />
+				</div>
+				<ul>{photos}</ul>
+			</div>
+		);
+	}
 }
 
-export default Gallery
+export default Gallery;
